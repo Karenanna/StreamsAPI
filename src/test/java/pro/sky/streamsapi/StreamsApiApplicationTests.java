affe68f84d29a8a30dd7c055162c3bd0ba112104
karen gevorgyan <karen-gev.22@mail.ru>
2023-05-01 13:32:18 +0300

    ДЗ Стримы

---
+++ src/test/java/pro/sky/streamsapi/StreamsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.streamsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
